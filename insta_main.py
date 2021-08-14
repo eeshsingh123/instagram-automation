c24f644de212488a9d6847defc4bb91cad0fbec0
eesh <eesh1996@gmail.com>
2021-08-14 12:11:57 +0530

    integrated end to end instaAutomate class

--- insta_main.py
+++ insta_main.py
@@ -19,9 +19,9 @@ class InstaAutomate:
             enabled=True,
             sleepyhead=True,
             notify_me=True,
-            peak_likes_hourly=70,
-            peak_likes_daily=680,
-            peak_comments_daily=240,
+            peak_likes_hourly=30,
+            peak_likes_daily=400,
+            peak_comments_daily=140,
             peak_comments_hourly=30,
             peak_follows_hourly=49,
             peak_unfollows_hourly=35,
@@ -33,23 +33,28 @@ class InstaAutomate:
 
     def like_posts_by_hashtags(self, hashtag_list, follow=False, comment=False):
 
-        self.session.set_smart_hashtags(['rakshabandhan', 'rakhi', 'rakhigift', 'rakhigifts'], limit=10, sort='top', log_tags=True)
+        # self.session.set_smart_hashtags(['rakshabandhan', 'rakhi', 'rakhigift', 'rakhigifts'], limit=10, sort='top', log_tags=True)
         print("MANUAL LOG-> DOING LIKE BY TAGS")
-        self.session.like_by_tags(amount=10, use_smart_hashtags=True)
-        self.session.like_by_tags(hashtag_list, amount=10)
-        if follow:
-            print("MANUAL LOG-> FOLLOWING USERS")
-            self.session.set_do_follow(True, percentage=80)
-        if comment:
-            print("MANUAL LOG-> COMMENTING ON USERS")
-            self.session.set_do_comment(True, percentage=80)
-            self.session.set_comments([
-                """Hey, We curate and deliver specialized Hampers dedicated to the auspicious events across the country, Do check out profile out >>>> ❤""",
-                "Hey, Looking for a Gift Hamper this Raksha Bandhan Season? Check our profile out. We have best Hampers at the most reasonable prices ❤",
-                "Hi, Check our profile out for the best Hampers at the best Prices >>> We are currently selling hampers for Raksha Bandhan",
-                "Hey, Great Picture!. Do check out profile out for the Best and Reasonable Hampers for Raksha Bandhan and many more events",
-                "Hey! Great Picture!, Visit our profile and DM us to get your hands on the Best Gift Hampers for your Loved ones."
-            ])
+        # self.session.like_by_tags(amount=10, use_smart_hashtags=True)
+        # self.session.like_by_tags(hashtag_list, amount=3)
+        self.session.set_do_follow(True, percentage=80)
+        self.session.set_do_comment(True, percentage=80)
+        self.session.set_comments([
+            """Hey, We curate and deliver Specialized Hampers dedicated to the auspicious events across the country, Do check out profile out >>>> ❤.""",
+            "Hey, Looking for a Gift Hamper this Raksha Bandhan Season? Check our profile out. We have best Hampers at the most reasonable prices ❤.",
+            "Hi, Check our profile out for the best Hampers at the best Prices >>> We are currently selling hampers for Raksha Bandhan.",
+            "Hey, Great Picture!, Do check our profile out for the Best and Reasonable Hampers for Raksha Bandhan and many more events.",
+            "Hey, Great Picture!, Visit our profile and DM us to get your hands on the Best Gift Hampers for your Loved ones."
+        ])
+        self.session.like_by_tags(hashtag_list, amount=3)
+        # self.session.set_do_comment(True, percentage=80)
+        # self.session.set_comments([
+        #     """Hey, We curate and deliver specialized Hampers dedicated to the auspicious events across the country, Do check out profile out >>>> ❤""",
+        #     "Hey, Looking for a Gift Hamper this Raksha Bandhan Season? Check our profile out. We have best Hampers at the most reasonable prices ❤",
+        #     "Hi, Check our profile out for the best Hampers at the best Prices >>> We are currently selling hampers for Raksha Bandhan",
+        #     "Hey, Great Picture!. Do check out profile out for the Best and Reasonable Hampers for Raksha Bandhan and many more events",
+        #     "Hey! Great Picture!, Visit our profile and DM us to get your hands on the Best Gift Hampers for your Loved ones."
+        # ])
 
         print("ENDING SESSION")
         self.session.end()
@@ -59,10 +64,11 @@ class InstaAutomate:
 
 
 if __name__ == "__main__":
+    htag_list = ['bhfyp', 'bhai', 'bhaibehen', 'rakshabandhan', 'happyrakshabandhan', 'rakhi', 'bestsister', 'photorakhi', 'rakhicelebration', 'independenceday', 'gift', 'rakhis', 'brothersisterlove', 'rakhi2021', 'hampers', 'rakshabandhangifts', 'rakhigifts', 'brotherandsister', 'siblings', 'bhai', 'rakshabandhanspecial', 'rakhigift']
     ia = InstaAutomate()
     ia.set_usage_quota()
     ia.like_posts_by_hashtags(
-        hashtag_list=['rakshabandhan', 'rakhi', 'rakhigift', 'rakhigifts'],
+        hashtag_list=htag_list,
         follow=True,
         comment=True
     )
